dcef6d9ee0fa4dd3c206fe7ac6eb4b73d5101374
ShikharSaraswat <67599312+ShikharSaraswat@users.noreply.github.com>
2021-04-24 20:08:28 +0530

    Update ApiService.js

--- src/Service/ApiService.js
+++ src/Service/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const USER_API_BASE_URL = 'http://localhost:8080/AllChemist';
+const USER_API_BASE_URL = 'https://allchemist10.herokuapp.com';
 
 class ApiService {
 
